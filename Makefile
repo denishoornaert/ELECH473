271826a11830dcbf11b13e6d503c881213009747
Detobel36 <detobel36@phoenix-rebirth.fr>
2018-02-18 16:11:24 +0100

    Add automatic tests on website :sunglasses:

--- Makefile
+++ Makefile
@@ -5,3 +5,9 @@ assignmentOne:
 
 clean:
 	cd assignment1 && $(MAKE) clean
+
+test:
+	cd assignment1 && $(MAKE) test
+
+install_requests:
+	pip3 install requests
--- convertAddr.py
+++ convertAddr.py
@@ -7,19 +7,16 @@
     Auto convert address
 """
 import sys
+import requests
+import webbrowser
+import time
 
-dictLabel = {}
+TEST_URL_ADD = 'http://164.15.75.7/'
 
-if len(sys.argv) <= 2:
-    if(len(sys.argv) > 1 and (sys.argv[1] == "help" or sys.argv[1] == "-h" or sys.argv[1] == "--help")):
-        print("Execute with: python " + sys.argv[0] + " <file to read> <file to produce>")
-        print("The program will replace '<? label ?>' from '<file to read>' to hex address to a file: '<file to produce>'")
+def convertLabel(sourceFile, destFile):
+    dictLabel = {}
 
-    else:
-        print("Error of argument: python " + sys.argv[0] + " <file to read> <file to produce>")
-
-else:
-    readFile = open(sys.argv[1], 'r')
+    readFile = open(sourceFile, 'r')
 
     contenu = readFile.read()
 
@@ -32,7 +29,7 @@ else:
         labelLine = line.split(":")
         if(len(labelLine) > 2):
             print("Multiple label Oo ! (line: " + str(counter) + " '" + line + "')")
-            sys.exit()
+            return None
 
         elif(len(labelLine) == 2):
             label = labelLine[0].strip()
@@ -47,9 +44,46 @@ else:
         if(splitLine[0] == "movi"):
             counter += 1
 
-    newFile = open(sys.argv[2], 'w')
+    newFile = open(destFile, 'w')
 
     for label in dictLabel:
         contenu = contenu.replace("<? " + label + " ?>", "0x{:04x}".format(dictLabel[label]))
 
     newFile.write(contenu)
+
+def testFile(sourceFile, exerciceName):
+    global TEST_URL_ADD
+
+    files = {'file': open(sourceFile, 'rb')}
+    data = {'exo': exerciceName, 'archi': 'IS0', 'logic': '0', 'asm_digest': '1', 'exec': '100000'}
+
+    r = requests.post(TEST_URL_ADD, files=files, data=data)
+
+    print("Work in progress...")
+    webbrowser.open_new_tab(r.url)
+
+
+if len(sys.argv) <= 2:
+    if(len(sys.argv) > 1 and (sys.argv[1] in ("help", "-h", "--help"))):
+        print("Execute with: python " + sys.argv[0] + " <file to read> <file to produce> <option>")
+        print("The program will replace '<? label ?>' from '<file to read>' to hex address to a file: '<file to produce>'")
+        print("Options:")
+        print("\t-t/--test [exercice]\tTest file online")
+        print("\t\t\t\tWhere [exercice] is the name of the exercice on the website (e.i. '1.5_b15_extract.txt')")
+
+    else:
+        print("[ERROR] Missing argument: python " + sys.argv[0] + " <file to read> <file to produce> <option>")
+        print("Execute: 'python " + sys.argv[0] + "help' for more infos")
+
+else:
+    convertLabel(sys.argv[1], sys.argv[2])
+    if(len(sys.argv) > 3):
+        if(sys.argv[3] in ("-t", "--test")):
+            if(len(sys.argv) >= 4):
+                testFile(sys.argv[2], sys.argv[4])
+            else:
+                print("[ERROR] Please precise the exercice name")
+        else:
+            print("[ERROR] Unknown paramter: " + sys.argv[3])
+
+
